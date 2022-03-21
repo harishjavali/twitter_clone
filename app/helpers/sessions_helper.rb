0117b27261bd5ae2cfa8dcc340cfee610d33afed
harishjavali <harish@shix.in>
2022-03-21 15:19:29 +0530

    Implemented adavnced login_chapter9

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,11 +2,31 @@ module SessionsHelper
     #logs in given user
     def log_in(user)
         session[:user_id] = user.id
+        # # Guard against session replay attacks.
+        # # See https://bit.ly/33UvK0w for more.
+        session[:session_token] = user.session_token
+    end
+    #Remember a user in a persistent session
+    def remember(user)
+        user.remember
+        cookies.permanent.encrypted[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
     end
     #to return current loggedin user if any
     def current_user
-        if session[:user_id]
-            @current_user ||=User.find_by(id: session[:user_id])
+        if (user_id = session[:user_id])
+            # @current_user ||= User.find_by(id: user_id)
+            user = User.find_by(id: user_id)
+            if user && session[:session_token] == user.session_token
+                @current_user = user
+            end
+        elsif (user_id = cookies.encrypted[:user_id])
+            # raise # The tests still pass, so this branch is currently untested.
+            user = User.find_by(id: user_id)
+            if user && user.authenticated?(cookies[:remember_token])
+                log_in user
+                @current_user = user
+            end
         end
     end
     #returns true if the user is logged in, fasle otherwise
@@ -14,8 +34,16 @@ module SessionsHelper
         !current_user.nil?
     end
 
+    #Forget a persistent session.
+    def forget(user)
+        user.forget
+        cookies.delete(:user_id)
+        cookies.delete(:remember_token)
+    end
+
     #logs out current user
     def log_out
+        forget(current_user)
         reset_session
         @current_user=nil
     end
